ce070dd522e474d334a48e2589c1633c8f1f0ed8
Matheus Dambrowski <matheusbd15@gmail.com>
2024-11-30 16:22:44 -0300

    Add reviews screen

--- app/(tabs)/my-reviews.tsx
+++ app/(tabs)/my-reviews.tsx
@@ -1,10 +1,44 @@
 import { Box } from "@/components/ui/box";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
+import { Icon, StarIcon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { myReviews } from "@/constants/reviews";
 
 export default function MyReviews() {
+  const reviews = myReviews;
   return (
-    <Box>
-      <Text>Reviews</Text>
+    <Box className="p-8">
+      <VStack>
+        <Heading className="mb-6">Minhas Análises</Heading>
+        {reviews.map((review) => (
+          <Card size="sm" variant="outline" className="mb-4">
+            <HStack space="lg" className="w-full">
+              <VStack>
+                <HStack className="justify-between w-full">
+                  <Heading>{review.title}</Heading>
+                  <HStack className="items-center flex-start" space="sm">
+                    <Text size="sm" className="pl-1 text-typography-900">
+                      {review.rating}
+                    </Text>
+                    <Icon
+                      as={StarIcon}
+                      size="2xs"
+                      className="stroke-typography-900 fill-typography-900"
+                    />
+                  </HStack>
+                </HStack>
+                <Text size="sm" className="text-typography-500">
+                  {review.reviewedPlace}
+                </Text>
+                <Text>{review.body}</Text>
+              </VStack>
+            </HStack>
+          </Card>
+        ))}
+      </VStack>
     </Box>
   );
 }
---
+++ constants/reviews.ts
@@ -0,0 +1,27 @@
+export type RestaurantReview = {
+  title: string;
+  body: string;
+  reviewedPlace: string;
+  rating: number;
+};
+
+export const myReviews: RestaurantReview[] = [
+  {
+    title: "Gostei do local",
+    body: "Comida muito boa e atendimento bom. Fui atendido pelo Fula de Tal.",
+    reviewedPlace: "Sukiyabashi Jiro",
+    rating: 4,
+  },
+  {
+    title: "Atendimento ruim",
+    body: "Fui muito mal atendido pelo garçom Fulano de Tal.",
+    reviewedPlace: "Cédric Grolet",
+    rating: 3,
+  },
+  {
+    title: "Perfeito",
+    body: "Comida perfeita, curti demais, recomendo muito o sushi de camarão.",
+    reviewedPlace: "Nusr-Et Steakhouse",
+    rating: 5,
+  },
+];
